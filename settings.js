cff890c6736438ac7014084a90033976822ffd52
osajixbotx <110822404+osajixbotx@users.noreply.github.com>
2022-08-08 14:59:10 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,9 +19,9 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
+global.owner = ['212615673973']
+global.premium = ['212615673973']
+global.ownernomer = '212615673973'
 global.ownername = '🐦Star Boy Nexus'
 global.botname = 'Gojo-Satoru'
 global.footer = '©NexusNwInc.'
